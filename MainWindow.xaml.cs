3d52f8aac2018a7bdf3f9a936bb651e8cc665503
rbaret <richardbaret@gmail.com>
2019-10-04 01:37:30 +0200

    First snippets of code to handle keyboard keys pressed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,79 @@ namespace Calculatrice
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string inputString = "";
+        private ulong inputULong = 0;
+        private double inputDouble = 0;
+        private double result = 0;
+        private bool isDecimal = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void parseInputString()
+        {
+            if (isDecimal)
+            {
+                inputDouble = double.Parse(inputString);
+            }
+            else
+                inputULong = ulong.Parse(inputString);
+
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            buildInputStringFromKeyboard(e.Key);
+        }
+        private void inputString_textCanged()
+        {
+
+        }
+        private void updateDisplay()
+        {
+
+        }
+        public void buildInputStringFromKeyboard(Key inputKey) {
+            switch (inputKey) {
+                case Key.D0:
+                    if (!String.IsNullOrEmpty(inputString))
+                        inputString.Append('0');
+                    break;
+                case Key.D1:
+                    inputString.Append('1');
+                    break;
+                case Key.D2:
+                    inputString.Append('2');
+                    break;
+                case Key.D3:
+                    inputString.Append('3');
+                    break;
+                case Key.D4:
+                    inputString.Append('4');
+                    break;
+                case Key.D5:
+                    inputString.Append('5');
+                    break;
+                case Key.D6:
+                    inputString.Append('6');
+                    break;
+                case Key.D7:
+                    inputString.Append('7');
+                    break;
+                case Key.D8:
+                    inputString.Append('8');
+                    break;
+                case Key.D9:
+                    inputString.Append('9');
+                    break;
+            }
+            textBoxHistory.Text = inputString;
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            inputString.Append('9');
         }
     }
 }
